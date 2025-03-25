1c5008c202cd4cd3fcc8192890e8e0c2b1fe9453
MinhMatthew <minhnca@gearment.com>
2025-03-25 13:04:15 +0700

    fix(landing): fix lib prisma define prisma client

--- apps/landing/src/lib/prisma.ts
+++ apps/landing/src/lib/prisma.ts
@@ -1,11 +1,8 @@
-// @ts-nocheck
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const prisma = new PrismaClient()
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
