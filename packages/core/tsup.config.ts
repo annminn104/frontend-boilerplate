e26de190acf2299b850553a7956329f7ee8c5732
MinhMatthew <minhnca@gearment.com>
2025-03-28 16:46:30 +0700

    fix(core): package core

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  splitting: false,
-  sourcemap: true,
   clean: true,
-  treeshake: true,
-  external: ['react'],
+  target: 'node16',
+  esbuildOptions(options) {
+    options.platform = 'node'
+  },
 })
