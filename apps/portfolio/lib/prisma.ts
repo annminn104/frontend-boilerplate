aaa722c09bc8adc8af27786da34f43ac1344b0ad
MinhMatthew <minhnca@gearment.com>
2025-03-31 14:51:06 +0700

    fix(landing): webhook clerk

--- apps/portfolio/lib/prisma.ts
+++ apps/portfolio/lib/prisma.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma }
+
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
