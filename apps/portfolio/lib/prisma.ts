a235539b3a73fb4a2ae8333ad90fe1e74c9cd713
MinhMatthew <minhnca@gearment.com>
2025-03-31 14:53:52 +0700

    fix(landing): webhook clerk

--- apps/portfolio/lib/prisma.ts
+++ apps/portfolio/lib/prisma.ts
@@ -1,10 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma }
+// Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate())
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
