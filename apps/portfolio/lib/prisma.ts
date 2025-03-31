9358d5b8706611b6ac0e3182769ddef015b9d591
MinhMatthew <minhnca@gearment.com>
2025-03-31 16:52:14 +0700

    fix(landing): prisma client

--- apps/portfolio/lib/prisma.ts
+++ apps/portfolio/lib/prisma.ts
@@ -9,7 +9,17 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+let prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (typeof window === 'undefined') {
+  if (process.env.NODE_ENV === 'production') {
+    prisma = prismaClientSingleton()
+  } else {
+    if (!global.prisma) {
+      global.prisma = prismaClientSingleton()
+    }
+  }
+}
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
